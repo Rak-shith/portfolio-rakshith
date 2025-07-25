7953e65fba86937e29b8282ecdf639c33d7973f3
Rakshith Kotian <rakshith.kotian@niveussolutions.com>
2025-07-25 15:43:37 +0530

    Adding global loader

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
+import { Suspense } from "react";
 import Hero from "./components/Hero";
 import Newsletter from "./components/Newsletter";
 import Projects from "./components/Projects";
-
+import GlobalLoading from "./loading";
 
 export default function Home() {
   return (
     <main>
-      <Hero />
-      <Projects />
-      <Newsletter />
+      <Suspense fallback={<GlobalLoading />}>
+        <Hero />
+        <Projects />
+        <Newsletter />
+      </Suspense>
     </main>
   );
-} 
+}
